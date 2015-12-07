0249ee9e1c44cfb6a3c8c366c3a5f145922f0f75
nomilous <nomilous@gmail.com>
2015-12-08 00:30:35 +0200

    1.0.4

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 node cli tool for tracing and reporting required files
 
+```sh
+
+sudo npm install -g node-required
+
+// -j outputs json
+
+node-required -j lib/file.js
+{
+  files: [
+    '/list/of/files.js',
+    '/required/by/the.js',
+    '/specified/file.js'
+  ]
+}
+
+```
---
+++ test/required_test.js
@@ -0,0 +1,70 @@
+var should = require('should');
+var path = require('path');
+var exec = require('child_process').exec;
+var required = path.normalize(__dirname + '/../bin/required');
+
+describe('node-required', function() {
+
+  it('ignores block comments', function(done) {
+    /*
+
+       require('nonexistant');
+
+     */
+
+     /* require('nonexistant') */
+
+     /***
+      *
+      * require('nonexistant')
+      *
+      ***/
+
+    // test with **this** file as target,
+    // does not fail on nonexistant
+
+    exec('node ' + required + ' ' + __filename, function(e, stdout) {
+      if (e) return done(e);
+      done();
+    });
+  });
+
+  it('ignores line comments', function(done) {
+
+    // require('nonexistant');
+
+    exec('node ' + required + ' ' + __filename, function(e, stdout) {
+      if (e) return done(e);
+      done();
+    });
+  });
+
+  it('lists required files as flat list', function(done) {
+    exec('node ' + required + ' ' + __filename, function(e, stdout) {
+      if (e) return done(e);
+      stdout.should.match(/should\/index.js/);
+      done();
+    });
+  });
+
+  it('lists required files as json', function(done) {
+    exec('node ' + required + ' -j ' + __filename, function(e, stdout) {
+      if (e) return done(e);
+      var parsed = JSON.parse(stdout);
+      parsed.files[0].should.match(/should\/index.js/)
+      done();
+    });
+  });
+
+  it('handles relative paths', function() {
+
+    var relative = require('./relative');
+
+    exec('node ' + required + ' ' + __filename, function(e, stdout) {
+      if (e) return done(e);
+      done();
+    });
+
+  });
+
+});
